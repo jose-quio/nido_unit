2bd1f34ef328681e237b5c7cec221887f0e6717a
Cristopher Gabriel Vera Quispe <cris.vera.quispe@gmail.com>
2025-04-12 12:26:36 -0500

    Creación de los componentes
    Creación de los componentes y servicios

---
+++ nidounit/src/app/apartamento-hab/apartamento-hab.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-apartamento-hab',
+  templateUrl: './apartamento-hab.component.html',
+  styleUrls: ['./apartamento-hab.component.scss']
+})
+export class ApartamentoHabComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- nidounit/src/app/app.module.ts
+++ nidounit/src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ApartamentoComponent } from './apartamento/apartamento.component';
+import { ApartamentoHabComponent } from './apartamento-hab/apartamento-hab.component';
+import { CajaComponent } from './caja/caja.component';
+import { PagoComponent } from './pago/pago.component';
+import { PropietarioComponent } from './propietario/propietario.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ApartamentoComponent,
+    ApartamentoHabComponent,
+    CajaComponent,
+    PagoComponent,
+    PropietarioComponent
   ],
   imports: [
     BrowserModule,
