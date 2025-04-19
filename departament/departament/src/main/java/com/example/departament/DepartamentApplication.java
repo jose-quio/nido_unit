54dad8c217f90c1eda8ecda7acacc24674669ff5
jose-quio <114790377+jose-quio@users.noreply.github.com>
2025-04-19 12:23:31 -0500

    create proyect back

---
+++ departament/departament/src/main/java/com/example/departament/DepartamentApplication.java
@@ -0,0 +1,14 @@
+package com.example.departament;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepartamentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepartamentApplication.class, args);
+		System.out.println("holamundo");
+	}
+
+}
