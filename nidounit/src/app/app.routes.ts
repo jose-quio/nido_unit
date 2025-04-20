2a3581f8de895b288fe601028e4edff3a6ebd83a
Cristopher Gabriel Vera Quispe <cris.vera.quispe@gmail.com>
2025-04-20 12:32:28 -0500

    Aplication Bootstrap and Main
    Componente App principal con diseño completo e implementación de bootstrap al proyecto

--- nidounit/src/app/app.routes.ts
+++ nidounit/src/app/app.routes.ts
@@ -5,12 +5,14 @@ import { CajaComponent } from './caja/caja.component';
 import { PagoComponent } from './pago/pago.component';
 import { PropietarioComponent } from './propietario/propietario.component';
 import { ApartamentoHabComponent } from './apartamento-hab/apartamento-hab.component';
+import { AppComponent } from './app.component';
 
 
 export const routes: Routes = [
+    {path: '', redirectTo: '/apartamento', pathMatch: 'full' },
     {path: 'apartamento', component: ApartamentoComponent},
     {path: 'apartamentoHab', component: ApartamentoHabComponent},
     {path: 'propietario', component: PropietarioComponent},
     {path: 'pago', component: PagoComponent},
-    {path: 'caja', component: CajaComponent}
+    {path: 'caja', component: CajaComponent},
 ];
