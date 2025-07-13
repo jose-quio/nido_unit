78d7687c066b91de53937cc31ffdb9c7aefa56e5
jose-quio <114790377+jose-quio@users.noreply.github.com>
2025-07-12 23:22:22 -0500

    update json ignore de Company

--- departament/departament/src/main/java/com/example/departament/Repository/EdificioRepository.java
+++ departament/departament/src/main/java/com/example/departament/Repository/EdificioRepository.java
@@ -1,11 +1,13 @@
 package com.example.departament.Repository;
 
+import com.example.departament.Entity.Departamento;
 import com.example.departament.Entity.Edificio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EdificioRepository extends JpaRepository<Edificio, Long> {
     @Query("SELECT e.id as id, e.nombre as nombre, e.nroPisos as nroPisos FROM Edificio e")
@@ -15,6 +17,9 @@ public interface EdificioRepository extends JpaRepository<Edificio, Long> {
 
     @Query("SELECT e.nroPisos FROM Edificio e WHERE e.id = :id")
     Integer findNroPisosById(@Param("id") Long id);
+
+
+
 
 
     interface EdificioProjection {
