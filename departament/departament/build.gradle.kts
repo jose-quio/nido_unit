473f9fa2bc5d61791b5d8163914fb5fb5d38da9c
jose-quio <114790377+jose-quio@users.noreply.github.com>
2025-07-22 23:13:02 -0500

    config database postgresql

--- departament/departament/build.gradle.kts
+++ departament/departament/build.gradle.kts
@@ -27,11 +27,15 @@ dependencies {
 
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
-
 	implementation("org.springframework.boot:spring-boot-starter-security")
 
+	//PostgreSQl Driver
+	implementation("org.postgresql:postgresql:42.7.3")
+
+	// Hibernate Dialect (opcional si quieres soporte extendido)
 	implementation("org.hibernate.orm:hibernate-community-dialects:6.6.11.Final")
-	implementation("org.xerial:sqlite-jdbc:3.42.0.0")
+
+	//implementation("org.xerial:sqlite-jdbc:3.42.0.0")
 	implementation("org.hibernate.validator:hibernate-validator")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
@@ -39,8 +43,10 @@ dependencies {
 	implementation("io.jsonwebtoken:jjwt:0.9.1")
 	implementation("javax.xml.bind:jaxb-api:2.3.0")
 
+	//Pdf y Correo
 	implementation("com.itextpdf:itext7-core:7.2.3")
 	implementation("org.springframework.boot:spring-boot-starter-mail")
+
 	testImplementation("org.springframework.security:spring-security-test")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
